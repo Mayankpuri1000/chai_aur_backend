0ac684f240af0bb031ba6e5e33530f4f7f500527
Mayankpuri1000 <mayankpuri1000@gmail.com>
2026-09-21 16:37:36 +0530

    Implement user and video models; update package.json and app structure

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "*", 
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}));
+app.use(cookieParser());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send(
+      "<body style='background-color: black; color: white'><h1>Welcome to Chai aur backend</h1></body>",
+    );
+})
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ dotenv.config({
 })
 
 import { connectDB } from "./db/index.js";
+import app from "./app.js";
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on http://localhost:${process.env.PORT || 8000}`)
+    })
+})
+.catch((err) => {
+    console.log("Error connecting to MongoDB: ", err);
+})
